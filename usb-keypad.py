de137825ef7b5ed2cb0a88df57f2e9475b42648f
Ward Witt <wardw@filmworkers.com>
2017-01-25 11:52:11 -0600

    Threadded

--- usb-keypad.py
+++ usb-keypad.py
@@ -1,4 +1,5 @@
-
+from evdev import InputDevice
+from threading import Thread
 
 keys = {
     2: "1",
@@ -10,20 +11,24 @@ keys = {
     8: "7",
     9: "8",
     10: "9",
-    11: "*",
+    11: "Lock",
     30: "0",
-    48: "#"
+    48: "Un-Lock"
     }
 
-from evdev import InputDevice
 
 DEVICE = "/dev/input/by-id/usb-Storm-Interface.com_Storm-Interface-event-kbd"
 
 dev = InputDevice(DEVICE)
 dev.grab()  #exclusive access
 
-for event in dev.read_loop():
-      if event.type==1 and event.value==1:
+def keyPadScan():
+   for event in dev.read_loop():
+       if event.type==1 and event.value==1:
          if event.code in keys:
-            print("KEY CODE: ", keys[event.code])
+            print("KEY CODE: ", keys[event.code], event.sec)
                
+
+keyPad = Thread(target = keyPadScan)
+
+keyPad.start()

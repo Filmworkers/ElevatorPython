097d8fbc3c4bf3ecc057383960f1f6278739a046
Ward Witt <wardw@filmworkers.com>
2017-01-27 10:10:40 -0600

    Making progress start time and inter-key time working

--- usb-keypad.py
+++ usb-keypad.py
@@ -1,5 +1,6 @@
 from evdev import InputDevice
 from threading import Thread
+from queue import Queue
 
 keys = {
     2: "1",
@@ -22,13 +23,39 @@ DEVICE = "/dev/input/by-id/usb-Storm-Interface.com_Storm-Interface-event-kbd"
 dev = InputDevice(DEVICE)
 dev.grab()  #exclusive access
 
-def keyPadScan():
+def keyPadScan(out_q):
    for event in dev.read_loop():
        if event.type==1 and event.value==1:
          if event.code in keys:
-            print("KEY CODE: ", keys[event.code], event.sec)
+            # print("KEY CODE: ", keys[event.code], event.sec)
+            out_q.put([keys[event.code], event.sec])
                
+def supervisor(in_q):
+    code = ""
+    preTime = 0
+    startTime = 0
+    while q.not_empty:
+        keypress = in_q.get()
+        key=keypress[0]
+        time=keypress[1]
+        if time - startTime > 10:
+           startTime=time
+           preTime=time
+           code=""
+           print()
+        if time - preTime < 3:
+            code+=key
+            preTime=time
 
-keyPad = Thread(target = keyPadScan)
+              
+        print(code, preTime, startTime)
+            
+      
+      
 
-keyPad.start()
+q = Queue()      
+keyPadScanThread = Thread(target=keyPadScan, args=(q,))
+supervisorThread = Thread(target=supervisor, args=(q,))
+
+supervisorThread.start()
+keyPadScanThread.start()

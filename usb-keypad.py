11791d4b5416445b147c6e70d9c66dd92801faf2
Ward Witt <wardw@filmworkers.com>
2017-01-27 15:51:45 -0600

    relays module

--- usb-keypad.py
+++ usb-keypad.py
@@ -2,16 +2,25 @@ from evdev import InputDevice
 from threading import Thread
 from queue import Queue
 from subprocess import call
+from relays import Relay
 import smbus
-
-bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
 code = ""
 preTime = 0
 startTime = 0
 keyCount = 0
 
-keys = {
+
+
+
+
+def keyPadScan(out_q):
+   DEVICE = "/dev/input/by-id/usb-Storm-Interface.com_Storm-Interface-event-kbd"
+
+   dev = InputDevice(DEVICE)
+   dev.grab()  #exclusive access
+
+   keys = {
     2: "1",
     3: "2",
     4: "3",
@@ -26,68 +35,6 @@ keys = {
     48: "Un-Lock"
     }
 
-
-DEVICE = "/dev/input/by-id/usb-Storm-Interface.com_Storm-Interface-event-kbd"
-
-dev = InputDevice(DEVICE)
-dev.grab()  #exclusive access
-
-class Relay():  
-    global bus
-    def __init__(self):
-        self.DEVICE_ADDRESS = 0x20    #7 bit address (will be left shifted to add the read write bit)
-        self.DEVICE_REG_MODE1 = 0x06
-        self.DEVICE_REG_DATA = 0xff
-        bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
-             
-    def ON_1(self):
-            print ('ON_1...')
-            self.DEVICE_REG_DATA &= ~(0x1<<0)  
-            bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
-    def ON_2(self):
-            print ('ON_2...')
-            self.DEVICE_REG_DATA &= ~(0x1<<1)
-            bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
-    def ON_3(self):
-            print ('ON_3...')
-            self.DEVICE_REG_DATA &= ~(0x1<<2)
-            bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
-    def ON_4(self):
-            print ('ON_4...')
-            self.DEVICE_REG_DATA &= ~(0x1<<3)
-            bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
-    
-    def OFF_1(self):
-            print ('OFF_1...')
-            self.DEVICE_REG_DATA |= (0x1<<0)
-            bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
-    
-    def OFF_2(self):
-            print ('OFF_2...')
-            self.DEVICE_REG_DATA |= (0x1<<1)
-            bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
-
-    def OFF_3(self):
-            print ('OFF_3...')
-            self.DEVICE_REG_DATA |= (0x1<<2)
-            bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
-    
-    def OFF_4(self):
-            print ('OFF_4...')
-            self.DEVICE_REG_DATA |= (0x1<<3)
-            bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
-    
-    def ALLON(self):
-            print ('ALLON...')
-            self.DEVICE_REG_DATA &= ~(0xf<<0)
-            bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
-    
-    def ALLOFF(self):
-            print ('ALLOFF...')
-            self.DEVICE_REG_DATA |= (0xf<<0)
-            bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
-
-def keyPadScan(out_q):
    for event in dev.read_loop():
        if event.type==1 and event.value==1:
          if event.code in keys:

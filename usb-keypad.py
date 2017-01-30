cab5cf6ed307956bccaf21fa5aad5a1117571af0
Ward Witt <wardw@filmworkers.com>
2017-01-30 11:25:08 -0600

    Refactroing and comments added

--- usb-keypad.py
+++ usb-keypad.py
@@ -3,10 +3,9 @@ from threading import Thread
 from queue import Queue
 from subprocess import call
 from relays import Relay
-import smbus
 
-code = ""
-preTime = 0
+enteredCode = ""
+intraKeyTime = 0
 startTime = 0
 keyCount = 0
 
@@ -37,56 +36,56 @@ def keyPadScan(out_q):
    for event in dev.read_loop():
        if event.type==1 and event.value==1:
          if event.code in keys:
-            # print("KEY CODE: ", keys[event.code], event.sec)
-            out_q.put([keys[event.code], event.sec])
+             out_q.put([keys[event.code], event.sec])
                
 def supervisor(in_q):
-    global code
-    global preTime
+    global enteredCode
+    global intraKeyTime
     global startTime
     global keyCount
 
     while True:
        if q.not_empty:
-           keypress = in_q.get()
-           key=keypress[0]
-           time=keypress[1]
-           keyCount +=1
-           if time - startTime > 5:
+           keypress=in_q.get()
+           key=keypress[0]  #Key the user hit
+           time=keypress[1] #when the user hit the key
+           keyCount+=1      #how many keys the user has hit
+           if time-startTime > 5: #if it's been more than 5 seconds
               clear()
-              preTime=time
+              intraKeyTime=time
               startTime=time
-           if time - preTime < 3:
-               code+=key
-               preTime=time
+           if time - intraKeyTime < 3:
+               enteredCode+=key
+               intraKeyTime=time
+               print("time out")
            if keyCount==6:
-              if code=="031775Lock":
+              if enteredCode=="031775Lock":
                  print("Third Floor Locked")
                  relay.OFF_3()
                  call(["omxplayer", "/home/pi/elevator/resource/Third lock.m4a"])
                  clear()
-              if code=="031775Un-Lock":
+              if enteredCode=="031775Un-Lock":
                  print("Third Floor Un-Locked")
                  relay.ON_3()
                  call(["omxplayer", "/home/pi/elevator/resource/Third unlock.m4a"])
                  clear()
-              if code=="041775Lock":
+              if enteredCode=="041775Lock":
                  relay.OFF_4()
                  print("Fourth Floor Locked")
                  call(["omxplayer", "/home/pi/elevator/resource/Penthouse lock.m4a"])
                  clear()
-              if code=="041775Un-Lock":
+              if enteredCode=="041775Un-Lock":
                  print("Fourth Floor Un-Locked")
                  relay.ON_4()
                  call(["omxplayer", "/home/pi/elevator/resource/Penthouse unlock.m4a"])
                  clear()
-              if code=="001775Un-Lock":
+              if enteredCode=="001775Un-Lock":
                  print("3&4 Un-Locked")
                  relay.ON_3()
                  relay.ON_4()
                  call(["omxplayer", "/home/pi/elevator/resource/Both unlock.m4a"])
                  clear()
-              if code=="001775Lock":
+              if enteredCode=="001775Lock":
                  print("3&4 Locked")
                  relay.OFF_3()
                  relay.OFF_4()
@@ -95,24 +94,25 @@ def supervisor(in_q):
                
            if keyCount>6:
               print("what")
-              call(["omxplayer", "/home/pi/elevator/resource/You don't know the code.m4a"])
+              call(["omxplayer", "/home/pi/elevator/resource/You don't know the enteredCode.m4a"])
               clear()
 
            if (key=="Un-Lock") | (key=="Lock"):
               if (keyCount>3) & (keyCount<6):
-                 call(["omxplayer", "/home/pi/elevator/resource/Whoopsy.m4a"])
+                 call(["omxplayer", "/home/pi/elevator/resource/Meh.m4a"])
                  clear()
               
       
 def clear():
-   global code
+   global enteredCode
    global keyCount
    global startTime
-   global preTime
-   code=""
+   global intraKeyTime
+   enteredCode=""
    keyCount=0
    startTime=0
-   preTime=0
+   intraKeyTime=0
+   #call(["omxplayer", "/home/pi/elevator/resource/Try again.m4a"])
 
 relay = Relay()
 q = Queue()      
